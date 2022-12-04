7272412fa29d89b20f9b276bd0905100e39da751
User <yasasvigumma@gmail.com>
2022-12-04 10:33:42 +0530

    this is a comment

---
+++ demo/print.java
@@ -0,0 +1,9 @@
+package demo;
+public class print{
+	public static void printer(){
+		System.out.println("Hello user!");
+	}
+	/*public static void main(String[] args){
+		printer();
+	}*/
+}
---
+++ foldertest/test.java
@@ -0,0 +1,7 @@
+import demo.*;
+public class test{
+	public static void main(String[] args){
+		print p = new print();
+		print.printer();
+	}
+}
